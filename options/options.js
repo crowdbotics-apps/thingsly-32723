395cce51b1b3ce2a98b49c6a7573668a0371e3e8
Crowdbotics <team@crowdbotics.com>
2022-01-07 04:12:04 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "thingsly_32723",
+  url: "https://thingsly_32723.botics.co",
+  api: "https://thingsly_32723.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
